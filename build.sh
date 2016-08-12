f31b48a91777cc4207882dcbccda6420437f8cbb
Ben Millwood <thebenmachine+git@gmail.com>
2016-08-13 00:30:12 +0800

    rename web.ml to observer.ml

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 ocamlbuild -use-ocamlfind -plugin-tag "package(ppx_driver.ocamlbuild)" \
-	figgie/figgie.byte web/web.byte
-js_of_ocaml +bin_prot.js +core_kernel.js +nat.js +weak.js web.byte
+	figgie/figgie.byte web/observer.byte
+js_of_ocaml +bin_prot.js +core_kernel.js +nat.js +weak.js observer.byte
