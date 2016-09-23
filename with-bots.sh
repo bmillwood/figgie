fef2f30fe27000cede7cf9267d44ccdabed8c594
Ben Millwood <thebenmachine+git@gmail.com>
2016-09-23 23:33:02 +0800

    helper script to simplify testing

---
+++ with-bots.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./figgie.byte server &
+victims=$!
+sleep 0.1
+for which in 1 2
+do
+  ./figgie.byte bot sell -server localhost:10203 \
+    -size 1 -fade 1 -at 6 \
+    -which $which &
+  victims="$victims $!"
+done
+./figgie.byte bot count -server localhost:10203 &
+victims="$victims $!"
+trap "kill $victims" EXIT
+wait
