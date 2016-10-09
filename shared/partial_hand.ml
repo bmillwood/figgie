4b8017f906cb63b0998073418ff3fa80d49e43ba
Ben Millwood <thebenmachine+git@gmail.com>
2016-10-10 01:22:41 +0800

    fix bug when someone sells unknown cards

--- shared/partial_hand.ml
+++ shared/partial_hand.ml
@@ -1,11 +1,13 @@
-type t = {
-  known   : Market.Size.t Card.Hand.t;
-  unknown : Market.Size.t;
-}
+open Market
+
+type t =
+  { known   : Size.t Card.Hand.t
+  ; unknown : Size.t
+  }
 
 let empty =
-  { known = Card.Hand.create_all Market.Size.zero
-  ; unknown = Market.Size.zero
+  { known = Card.Hand.create_all Size.zero
+  ; unknown = Size.zero
   }
 
 let create_unknown size = { empty with unknown = size }
@@ -14,7 +16,7 @@ let create_known hand = { empty with known = hand }
 
 let selling t ~suit ~size =
   let old_size = Card.Hand.get t.known ~suit in
-  let open Market.Size.O in
+  let open Size.O in
   if size <= old_size
   then t
   else
@@ -23,12 +25,19 @@ let selling t ~suit ~size =
     }
 
 let bought t ~suit ~size =
-  let known = Card.Hand.modify t.known ~suit ~f:(Market.Size.(+) size) in
+  let known = Card.Hand.modify t.known ~suit ~f:(Size.(+) size) in
   { t with known }
 
-let sold t ~suit ~size = bought t ~suit ~size:(Market.Size.neg size)
+let sold t ~suit ~size:size_sold =
+  let known_sold = Size.min size_sold (Card.Hand.get t.known ~suit) in
+  let unknown_sold = Size.O.(size_sold - known_sold) in
+  let known =
+    Card.Hand.modify t.known ~suit ~f:(fun s -> Size.O.(s - known_sold))
+  in
+  let unknown = Size.O.(t.unknown - unknown_sold) in
+  { known; unknown }
 
 let traded t ~suit ~size ~dir =
-  Market.Dir.fold dir ~buy:bought ~sell:sold t ~suit ~size
+  Dir.fold dir ~buy:bought ~sell:sold t ~suit ~size
 
 let unknown_utf8 = "\xe2\x96\x88"
