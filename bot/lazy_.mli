5806a64896532dff1ca235094877c33412179c5b
Ben Millwood <thebenmachine+git@gmail.com>
2017-02-11 18:31:15 +0800

    delete lazybot

--- bot/lazy_.mli
+++
@@ -1,3 +0,0 @@
-open Core.Std
-
-val command : Command.t
--- bot/main.ml
+++ bot/main.ml
@@ -3,8 +3,7 @@ open Core.Std
 ;;
 Command.group
   ~summary:"Run a bot"
-  [ "lazy",  Lazy_.command
-  ; "sell",  Sell.command
+  [ "sell",  Sell.command
   ; "count", Count.command
   ]
 |> Command.run
