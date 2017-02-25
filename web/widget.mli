2ff438124dac4437a4892d2527564822a609ff90
Ben Millwood <thebenmachine+git@gmail.com>
2017-02-25 17:38:24 +0800

    another mli

---
+++ web/widget.mli
@@ -0,0 +1,18 @@
+open! Core_kernel.Std
+open Incr_dom.Std
+open Vdom
+
+val textbox
+  :  ?id:string
+  -> ?classes:string list
+  -> ?placeholder:string
+  -> ?initial_value:string
+  -> ?disabled:bool
+  -> ?clear_on_submit:bool
+  -> ?on_keypress:(
+       self:Dom_html.inputElement Js.t
+    -> Dom_html.keyboardEvent Js.t
+    -> Event.t)
+  -> on_submit:(string -> Event.t)
+  -> unit
+  -> Node.t
