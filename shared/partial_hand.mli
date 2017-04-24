3e84581c4b72ba37882556ac6e03da3bb323d7dc
Ben Millwood <thebenmachine+git@gmail.com>
2017-04-24 11:37:04 +0800

    don't need to expose Partial_hand.{bought,sold}

--- shared/partial_hand.mli
+++ shared/partial_hand.mli
@@ -11,6 +11,4 @@ val create_unknown : Size.t             -> t
 val create_known   : Size.t Card.Hand.t -> t
 
 val selling : t -> suit:Card.Suit.t -> size:Size.t -> t
-val bought  : t -> suit:Card.Suit.t -> size:Size.t -> t
-val sold    : t -> suit:Card.Suit.t -> size:Size.t -> t
 val traded  : t -> suit:Card.Suit.t -> size:Size.t -> dir:Dir.t -> t
