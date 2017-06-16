d5d0bdf9119d8bfd75d8c53f4eb928981be07f24
Ben Millwood <thebenmachine+git@gmail.com>
2017-06-16 19:25:44 +0800

    lobby_view.mli

---
+++ web/lobby_view.mli
@@ -0,0 +1,22 @@
+open! Core_kernel.Std
+open Incr_dom
+open Vdom
+
+open Figgie
+
+module Action : sig
+  module Room_action : sig
+    type t =
+      | Create
+      | Join
+      | Delete
+  end
+
+  type t = { room_id : Lobby.Room.Id.t; action : Room_action.t }
+end
+
+val view
+  :  Lobby.t
+  -> my_name:Username.t
+  -> inject:(Action.t -> Event.t)
+  -> Node.t
