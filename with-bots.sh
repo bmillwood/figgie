50582ab4c8b6a96265cf40f9673558cbd9709432
Ben Millwood <thebenmachine+git@gmail.com>
2017-07-01 20:53:00 +0800

    make it easier to choose bots in with-bots.sh

--- with-bots.sh
+++ with-bots.sh
@@ -11,11 +11,17 @@ BOT=$BUILD/bot/main.exe
 victims=$!
 trap 'kill $victims' EXIT
 sleep 1
-for ty in sell chaos count
-do
-  "$BOT" $ty -server localhost:58828 -log-level Debug \
+
+function bot() {
+  ty=$1
+  shift
+  "$BOT" "$ty" -server localhost:58828 -log-level Debug "$@" \
     2>&1 | sed -re "s/^/$ty /" &
   victims="$victims $!"
   sleep 3
-done
+}
+
+bot count
+bot chaos
+bot sell
 wait
